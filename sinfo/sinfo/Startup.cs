4fdabf9f8cea3c0caa356cb0520546053bde3a3a
Darwin <jhondarwinvasq@gmail.com>
2018-03-08 14:07:36 -0500

    Inicial

---
+++ sinfo/sinfo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sinfo.Startup))]
+namespace sinfo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
